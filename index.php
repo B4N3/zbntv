2d7b5cb2dc43c213980b771606b8ec0dc0ca53fa
B4N3 <B4N3@users.noreply.github.com>
2021-02-27 18:13:31 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-//header('Content-Type: application/json');
-//$filename = '1.txt';
-//$username = "refresh";
-//$username = "signin";
-//$code = $_GET['user'];
+
+
+
+
+
 echo "O arquivo ooi existe";
 
 ?>
